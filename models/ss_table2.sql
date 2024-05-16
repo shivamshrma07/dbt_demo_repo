84db074f396f08fe4e6c37ce95626ceb8009338d
Shivam Sharma <shivam.r.sharma@zs.com>
2024-05-16 10:08:38 +0000

    c

--- models/ss_table2.sql
+++ models/ss_table2.sql
@@ -1,2 +1,2 @@
 {{ config(materialized='table') }}
-select cc_class as cc_class, sum(cc_mkt_id) as count from cc_class_combined where cc_class = '{{var_1('class') }}' group by cc_class
+select cc_class as cc_class, sum(cc_mkt_id) as count from cc_class_combined where cc_class = '{{var_1() }}' group by cc_class
