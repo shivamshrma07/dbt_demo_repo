07682c726152ec262fe4af37f181f1a4bb60d44b
Shivam Sharma <shivam.r.sharma@zs.com>
2024-05-16 11:21:24 +0000

    c

--- models/ss_table2.sql
+++ models/ss_table2.sql
@@ -1,2 +1,2 @@
 {{ config(materialized='table') }}
-select cc_class as cc_class, sum({{convert_to_multiple_of_10(cc_mkt_id)}}) as count from cc_class_combined where cc_class = '{{var('class') }}' group by cc_class
+select cc_class as cc_class, sum({{convert_to_multiple_of_10('cc_mkt_id')}}) as count from cc_class_combined where cc_class = '{{var('class') }}' group by cc_class
