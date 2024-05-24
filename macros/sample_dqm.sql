4eda68d765c4e5d1c08b97eff7880dcd455d3c4a
Shivam Sharma <shivam.r.sharma@zs.com>
2024-05-24 06:48:30 +0000

    aa

--- macros/sample_dqm.sql
+++ macros/sample_dqm.sql
@@ -3,7 +3,7 @@
     {% for dataset_id in dataset_ids %}
         {% set dqm_ids = run_query('select distinct dqm_id from {{var(dqm_db)}}.{{var(dqm_schema)}}.{{var(dqm_config_table)}} where dataset_id = {{ref(dataset_id)}}') %}
         {% for dqm_id in dqm_ids %}
-            select dqm_id  
+            select dqm_id 
         {% endfor %}
     {% endfor %}
 {% endmacro %}
